e28c885145ad992308806cdf541ef226366e79f7
Mariana Sickler <marianaleal_93@hotmail.com>
2017-10-11 14:14:05 +0000

    automatically backed up by learn

--- app/controllers/student_controller.rb
+++ app/controllers/student_controller.rb
@@ -1,5 +1,6 @@
 class StudentController < ApplicationController
-  def index
-    @students = Student.all
-  end
-end
+   def index
+     @students = Student.all
+     #explicitly, render "index"
+   end
+ end
